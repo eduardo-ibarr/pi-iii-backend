740243f929e18631fb88794ebca90e1c928828d5
Eduardo de Paula <eduardoibarr56@gmail.com>
2023-04-30 18:02:42 -0300

    feat: scripts modified

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-cd /var/www/backend-pi-iii/
+set -e
+
+cd /var/www/backend-pi-iii
+
+# Install dependencies
+echo "Installing dependencies"
 npm install
 
-screen -d -m npm start
+# Start the server in a screen session
+echo "Starting server in a screen session"
+screen -d -m -S backend npm start
+
+# Wait for server to start
+echo "Waiting for server to start"
+n=0
+until [ $n -ge 10 ]
+do
+    RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:3333)
+    if [ $RESPONSE -eq 200 ]; then
+        echo "Server started successfully"
+        break
+    else
+        echo "Server not yet ready, waiting..."
+        n=$[$n+1]
+        sleep 5
+    fi
+done
 
 exit 0
