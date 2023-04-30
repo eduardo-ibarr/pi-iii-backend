3c92cbd489463b69e2f2293cb50eae84ff7f079f
Eduardo de Paula <eduardoibarr56@gmail.com>
2023-04-30 14:18:34 -0300

    fix: fixing path and removing scripts comments

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Change to project directory
 cd /var/www/backend/
-
-# Install project dependencies
 npm install
-
-# Start Node.js server
 npm start
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Stop Apache service
 service httpd stop
-
-# Remove old project files
 rm -rf /var/www/backend-pi-iii/*
