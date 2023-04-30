cb4c432a30bcf887e8196649f211bd17adcd5a36
Eduardo de Paula <eduardoibarr56@gmail.com>
2023-04-30 17:36:19 -0300

    fix: executing npm start on 2nd plan

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,6 +2,8 @@
 
 cd /var/www/backend-pi-iii/
 npm install
-npm start &
+
+# Executa a aplicação em segundo plano e redireciona a saída para o arquivo "app.log"
+nohup npm start > app.log 2>&1 &
 
 exit 0
