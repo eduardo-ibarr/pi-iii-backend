1d020e040940aba3bfcd7665114ab43f48c775be
Eduardo de Paula <eduardoibarr56@gmail.com>
2023-05-04 19:47:37 -0300

    docs: adding docs for the agent-ticket-history module

--- src/modules/agent-ticket-history/infra/http/routes/agentTicketHistories.routes.ts
+++ src/modules/agent-ticket-history/infra/http/routes/agentTicketHistories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-import { AgentTicketHistoriesController } from '../controllers/AgentTicketHistoriesRepository';
-
 import { celebrate, Joi, Segments } from 'celebrate';
+
+import { AgentTicketHistoriesController } from '../controllers/AgentTicketHistoriesRepository';
 
 const router = Router();
 
@@ -13,6 +13,25 @@ router.get(
   () => {
     // #swagger.tags = ['Agent-Ticket History']
     // #swagger.description = 'Endpoint para obter uma lista um históticos de Agent-Ticket.'
+    /* #swagger.responses[200] = {
+        description: 'Históricos obtidos com sucesso.',
+        schema: [
+          {
+            id: '6c7a6bb7-8f08-4df0-b514-e6adaf9e35a7',
+            agent_id: '812af9f9-8538-4831-af5e-fcfd65ad45bf',
+            ticket_id: 'c22b8ecd-7cd0-49ed-af03-3e0efa447e5f',
+            created_at: new Date().toISOString(),
+            updated_at: new Date().toISOString(),
+          }
+        ]
+    } */
+    /* #swagger.responses[500] = {
+        description: 'Algum erro de servidor.',
+        schema: {
+          status: 'error',
+          message: 'Internal server error',
+        }
+  } */
   }
 );
 
@@ -27,6 +46,31 @@ router.get(
   () => {
     // #swagger.tags = ['Agent-Ticket History']
     // #swagger.description = 'Endpoint para obter um histótico de Agent-Ticket.'
+    // #swagger.parameters['id'] = { description: 'Id do histórico.', in: 'path', required: true }
+    /* #swagger.responses[200] = {
+      description: 'Histórico obtido com sucesso.',
+      schema: {
+            id: '6c7a6bb7-8f08-4df0-b514-e6adaf9e35a7',
+            agent_id: '812af9f9-8538-4831-af5e-fcfd65ad45bf',
+            ticket_id: 'c22b8ecd-7cd0-49ed-af03-3e0efa447e5f',
+            created_at: new Date().toISOString(),
+            updated_at: new Date().toISOString(),
+        }
+    } */
+    /* #swagger.responses[404] = {
+        description: 'Histórico não encontrado.',
+        schema: {
+          status: 'error',
+          message: 'Agent-Ticket history not found.',
+        }
+    } */
+    /* #swagger.responses[500] = {
+        description: 'Algum erro de servidor.',
+        schema: {
+          status: 'error',
+          message: 'Internal server error.',
+        }
+    } */
   }
 );
 
@@ -41,7 +85,57 @@ router.post(
   agentTicketHistoriesController.store,
   () => {
     // #swagger.tags = ['Agent-Ticket History']
-    // #swagger.description = 'Endpoint para criar um histótico de Agent-Ticket.'
+    // #swagger.description = 'Endpoint para criar um histórico de Agent-Ticket.'
+    // #swagger.parameters['id'] = { description: 'Id do histórico.', in: 'path', required: true }
+    /* #swagger.parameters['obj'] = {
+            in: 'body',
+            description: 'Exemplo de body para criar um histórico.',
+            required: true,
+            schema: { 
+              agent_id: '812af9f9-8538-4831-af5e-fcfd65ad45bf',
+              ticket_id: 'c22b8ecd-7cd0-49ed-af03-3e0efa447e5f',
+            }
+    } */
+    /* #swagger.responses[201] = {
+      description: 'Histórico criado com sucesso.',
+      schema: [
+        {
+            id: '6c7a6bb7-8f08-4df0-b514-e6adaf9e35a7',
+            agent_id: '812af9f9-8538-4831-af5e-fcfd65ad45bf',
+            ticket_id: 'c22b8ecd-7cd0-49ed-af03-3e0efa447e5f',
+            created_at: new Date().toISOString(),
+            updated_at: new Date().toISOString(),
+        }
+      ]
+    } */
+    /* #swagger.responses[404.1] = {
+        description: 'Histórico não encontrado.',
+        schema: {
+          status: 'error',
+          message: 'Agent-Ticket history not found.',
+        }
+    } */
+    /* #swagger.responses[404.2] = {
+        description: 'Agente informado não existe.',
+        schema: {
+          status: 'error',
+          message: 'The agent informed does not exists.',
+        }
+    } */
+    /* #swagger.responses[404.3] = {
+        description: 'Ticket informado não existe.',
+        schema: {
+          status: 'error',
+          message: 'The ticket informed does not exists.',
+        }
+    } */
+    /* #swagger.responses[500] = {
+        description: 'Algum erro de servidor.',
+        schema: {
+          status: 'error',
+          message: 'Internal server error.',
+        }
+    } */
   }
 );
 
@@ -60,6 +154,56 @@ router.put(
   () => {
     // #swagger.tags = ['Agent-Ticket History']
     // #swagger.description = 'Endpoint para atualizar um histótico de Agent-Ticket.'
+    // #swagger.parameters['id'] = { description: 'Id do histórico.', in: 'path', required: true }
+    /* #swagger.parameters['obj'] = {
+            in: 'body',
+            description: 'Exemplo de body para atualizar um histórico.',
+            required: true,
+            schema: { 
+              agent_id: '812af9f9-8538-4831-af5e-fcfd65ad45bf',
+              ticket_id: '85315bda-3719-490d-b780-1b7e64e027f9',
+            }
+    } */
+    /* #swagger.responses[201] = {
+      description: 'Histórico atualizado com sucesso.',
+      schema: [
+        {
+            id: '6c7a6bb7-8f08-4df0-b514-e6adaf9e35a7',
+            agent_id: '812af9f9-8538-4831-af5e-fcfd65ad45bf',
+            ticket_id: '85315bda-3719-490d-b780-1b7e64e027f9',
+            created_at: new Date().toISOString(),
+            updated_at: new Date().toISOString(),
+        }
+      ]
+    } */
+    /* #swagger.responses[404.1] = {
+        description: 'Histórico não encontrado.',
+        schema: {
+          status: 'error',
+          message: 'Agent-Ticket history not found.',
+        }
+    } */
+    /* #swagger.responses[404.2] = {
+        description: 'Agente informado não existe.',
+        schema: {
+          status: 'error',
+          message: 'The agent informed does not exists.',
+        }
+    } */
+    /* #swagger.responses[404.3] = {
+        description: 'Ticket informado não existe.',
+        schema: {
+          status: 'error',
+          message: 'The ticket informed does not exists.',
+        }
+    } */
+    /* #swagger.responses[500] = {
+        description: 'Algum erro de servidor.',
+        schema: {
+          status: 'error',
+          message: 'Internal server error.',
+        }
+    } */
   }
 );
 
@@ -74,6 +218,24 @@ router.delete(
   () => {
     // #swagger.tags = ['Agent-Ticket History']
     // #swagger.description = 'Endpoint para deletar um histótico de Agent-Ticket.'
+    // #swagger.parameters['id'] = { description: 'Id do histórico.', in: 'path', required: true }
+    /* #swagger.responses[200] = {
+      description: 'Histórico deletado com sucesso.',
+    } */
+    /* #swagger.responses[404] = {
+        description: 'Histórico não encontrado.',
+        schema: {
+          status: 'error',
+          message: 'Agent-Ticket history not found.',
+        }
+    } */
+    /* #swagger.responses[500] = {
+        description: 'Algum erro de servidor.',
+        schema: {
+          status: 'error',
+          message: 'Internal server error.',
+        }
+    } */
   }
 );
 
